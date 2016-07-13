b998204678e030457e6e4821482cace059f06a66
Kevin Fisher <kevinfisher@kevins-mbp.attlocal.net>
2016-07-12 22:06:15 -0500

    7/12/16

--- eventpage.js
+++ eventpage.js
@@ -1,7 +1,39 @@
+var howFar = {
+	origin: "",
+	destination: ""
+};
+
 function initializeContextMenu() {
 	var parentContextMenuItem = chrome.contextMenus.create({"title": "HowFar", "contexts": ["selection"], "id": "parentContextMenuItem"});
 	var setAsOriginContextMenuItem = chrome.contextMenus.create({"title": "Set as origin", "id": "setAsOriginContextMenuItem", "parentId": "parentContextMenuItem", "contexts": ["selection"]});
 	var setAsDestinationContextMenuItem = chrome.contextMenus.create({"title": "Set as destination", "id": "setAsDestinationContextMenuItem", "parentId": "parentContextMenuItem", "contexts": ["selection"]});
 }
 
-chrome.runtime.onInstalled.addListener(initializeContextMenu());
+function onContextMenuClicked(info, tab) {
+	if (info.menuItemId === "setAsOriginContextMenuItem") {
+		console.log("Context menu item clicked: \"setAsOriginContextMenuItem\"");
+		howFar.origin = info.selectionText;
+		console.log("Origin set to: " + howFar.origin);
+	}
+	else if (info.menuItemId === "setAsDestinationContextMenuItem") {
+		console.log("Context menu item clicked: \"setAsDestinationContextMenuItem\"");
+		howFar.destination = info.selectionText
+		console.log("Destination set to: " + howFar.destination);
+	}
+}
+
+function onBrowserActionClicked() {
+	console.log("onBrowserActionClicked");
+	if (howFar.origin !== "") {
+		console.log("onBrowserActionClicked howFar.origin");
+		$("#origin-input")[0].value = howFar.origin;
+	}
+	else if (howFar.destination !== "") {
+		console.log("onBrowserActionClicked howFar.destination");
+		$("#destination-input")[0].value = howFar.destination;
+	}
+}
+
+chrome.runtime.onInstalled.addListener(initializeContextMenu);
+chrome.contextMenus.onClicked.addListener(onContextMenuClicked);
+chrome.browserAction.onClicked.addListener(onBrowserActionClicked);
--- popup.js
+++ popup.js
@@ -1,21 +1,25 @@
 var howFar = {
-	parentItem: 0,
-	originItem: 0
+	origin: "origin",
+	destination: "destination"
 };
 
-document.onkeypress = function(e) {
+$(document).keypress(function(e) {
 	if (e.keyCode == 13) {
-		var mapDiv = $( "#debug" );
-		alert(mapDiv);
-		//var mapDiv = document.getElementById("map");
-		var originInput = document.getElementById("origin-input");
+		var mapDiv = $("#map")[0];
+		//var originInput = $("#origin-input")[0];
+		//console.log("Origin: " + originInput.value);
+		howFar.origin = $("#origin-input")[0].value;
+		console.log("Origin: " + howFar.origin);
+
 		mapDiv.setAttribute("height", "500");
 		mapDiv.setAttribute("width", "500");
 		var srcAttribute = document.createAttribute("src");
 		srcAttribute.value = "https://www.google.com/maps/embed/v1/directions?key=AIzaSyDemdTKRlYMUKo29J4fNqnAmkgmEiK_AnA&origin=Lincoln+Park+Zoo,+North+Clark+Street,+Chicago,+IL&destination=Willis+Tower,+South+Wacker+Drive,+Chicago,+IL";
 		mapDiv.setAttributeNode(srcAttribute);
+		
 	}
-}
+});
+
 
 
 
